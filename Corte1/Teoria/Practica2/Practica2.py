38b54ff9563719c844384a5f21d794a3d2b769aa
Juanca0014 <jucamilo2004@gmail.com>
2026-02-05 14:27:54 -0500

    Add files via upload

---
+++ Corte1/Teoria/Practica2/Practica2.py
@@ -0,0 +1,21 @@
+n1 = 3
+n2 = 3.0
+n3 = "3"
+#metodo 1
+print()
+print("Metodo 1: multiples argumentos")
+print("n1 es: ",type(n1))
+print("n2 es: ",type(n2))
+print("n3 es: ",type(n3))
+#metodo 2
+print()
+print("Metodo 2: convertir a string")
+print("n1 es: "+str(type(n1)))
+print("n2 es: "+str(type(n2)))
+print("n3 es: "+str(type(n3)))
+#metodo 3
+print()
+print("Metodo 3: f-string")
+print(f"n1 es: {type(n2)}")
+print(f"n2 es: {type(n2)}")
+print(f"n3 es: {type(n3)}")

1f140a9d1e6f076b10eb74b460898c411e9db6af
Juanca0014 <jucamilo2004@gmail.com>
2026-02-12 00:36:31 -0500

    Update practica1.cpp

--- Corte1/Teoria_cpp/Practica1/practica1.cpp
+++ Corte1/Teoria_cpp/Practica1/practica1.cpp
@@ -3,7 +3,8 @@
 int main() { // Función principal donde comienza la ejecución
     std::cout << "Hola, mundo!" << std::endl; // Imprime el texto en la consola y añade un salto de línea
     
-    system("pause"); // Pausar para que al ejecutar desde el .exe no se cierre automaticamente
+    std::cin.get(); // Pausar para que al ejecutar desde el .exe no se cierre automaticamente
     
 	return 0; // Indica que el programa terminó correctamente
+
 }

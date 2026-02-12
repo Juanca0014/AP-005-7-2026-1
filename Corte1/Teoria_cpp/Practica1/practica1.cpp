43196e9745ecf887e4b6a75eff98e44a963efd4a
Juanca0014 <jucamilo2004@gmail.com>
2026-02-11 22:09:53 -0500

    Add files via upload

---
+++ Corte1/Teoria_cpp/Practica1/practica1.cpp
@@ -0,0 +1,9 @@
+#include <iostream> // Incluye la biblioteca de entrada/salida de flujo
+
+int main() { // Función principal donde comienza la ejecución
+    std::cout << "Hola, mundo!" << std::endl; // Imprime el texto en la consola y añade un salto de línea
+    
+    system("pause"); // Pausar para que al ejecutar desde el .exe no se cierre automaticamente
+    
+	return 0; // Indica que el programa terminó correctamente
+}
